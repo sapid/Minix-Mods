1c6b2dc28c7feec1de8fddb7921cf7bd1fc0f4ab
Will Crawford <will@metawhimsy.com>
2011-04-08 20:34:17 -0700

    Created a fork test. Looks successful.

---
+++ 1/sandbox/forktest.c
@@ -0,0 +1,19 @@
+/* Author: Will Crawford <wacrawfo@ucsc.edu> */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int *return_status;
+int main(int argc, char *argv[], char *envp[]) {
+	printf("$ \n");
+	int pid = fork();
+	if(pid){
+		// In parent.
+		printf("Waiting for process...\n");
+		pid = wait(return_status);
+		printf("Returned from process.\n");
+	} else {
+		// In child.
+		printf("Child reporting.\n");
+	}
+	return 0;
+}
