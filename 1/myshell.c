7b257448c1fa430df0527051be330ebc2425d5d0
Will Crawford <will@metawhimsy.com>
2011-04-08 18:34:16 -0700

    This is garbage right now.

--- 1/myshell.c
+++ 1/myshell.c
@@ -1,3 +1,4 @@
+/* Author: Will Crawford <wacrawfo@ucsc.edu> */
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -5,12 +6,14 @@
 
 extern char **getline();
 
-int main() {
+int main(int argc, char *argv[], char *envp[]) {
   int i;
   char **args; 
 
   while(1) {
+    printf("$ ");
     args = getline();
+	 parse_args(args);
     for(i = 0; args[i] != NULL; i++) {
       printf("Argument %d: %s\n", i, args[i]);
     }
