4af78dcd47e890bac18c681cb1bfa5330d4879b2
Will Crawford <will@metawhimsy.com>
2011-04-09 22:11:57 -0700

    Finished a pipe test.

---
+++ 1/sandbox/pipetest.c
@@ -0,0 +1,30 @@
+/* Author: Will Crawford <wacrawfo@ucsc.edu> */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int *return_status;
+int main(int argc, char *argv[], char *envp[]) {
+	int fd[2];
+	printf("$ \n");
+	if(pipe(fd)){
+		fprintf(stderr, "Pipe failed! How mysterious.\n");
+		return 1;
+	}
+	int pid = fork();
+	if(pid){
+		// In parent.
+		close(fd[0]);
+		close(1);
+		dup(fd[1]);
+		printf("Talking through pipe.\n");
+	} else {
+		// In child.
+		close(fd[1]);
+		close(0);
+		dup(fd[0]);
+		int c;
+		while((c = getchar()) != EOF)
+			putchar(c);
+	}
+	return 0;
+}
